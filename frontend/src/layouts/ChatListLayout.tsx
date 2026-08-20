60888f60b69468b50d09b46fe96b64084761bc83
Diana Laura Gonzalez Escamilla <dianathcb@gmail.com>
2026-08-19 18:50:14 -0700

    Make sidebar on desktop, bottom nav on mobile

--- frontend/src/layouts/ChatListLayout.tsx
+++ frontend/src/layouts/ChatListLayout.tsx
@@ -4,14 +4,16 @@ import BottomNav from '../components/BottomNav';
 
 const ChatListLayout = () => {
   return (
-    <div className="h-screen overflow-hidden flex flex-col bg-surface">
-      <div className="shrink-0">
-        <ChatListHeader />
+    <div className="h-screen overflow-hidden flex flex-col bg-surface md:flex-row">
+      <div className="flex-1 flex flex-col min-w-0 overflow-y-auto order-1 md:order-2">
+        <div className="shrink-0">
+          <ChatListHeader />
+        </div>
+        <main className="flex-1 overflow-y-auto">
+          <Outlet />
+        </main>
       </div>
-      <main className="flex-1 overflow-y-auto">
-        <Outlet />
-      </main>
-      <div className="shrink-0">
+      <div className="order-2 md:order-1">
         <BottomNav />
       </div>
     </div>
--- frontend/src/layouts/PageLayout.tsx
+++ frontend/src/layouts/PageLayout.tsx
@@ -4,14 +4,17 @@ import Header from '../components/Header';
 
 const PageLayout = () => {
   return (
-    <div className="h-screen overflow-hidden flex flex-col bg-surface">
-      <div className="shrink-0">
-        <Header />
+    <div className="h-screen overflow-hidden flex flex-col md:flex-row bg-surface ">
+      <div className="flex-1 flex flex-col min-w-0 overflow-y-auto order-1 md:order-2">
+        <div className="shrink-0">
+          <Header />
+        </div>
+        <main className="flex-1 overflow-y-auto">
+          <Outlet />
+        </main>
       </div>
-      <main className="flex-1 overflow-y-auto">
-        <Outlet />
-      </main>
-      <div className="shrink-0">
+
+      <div className="order-2 md:order-1 shrink-0">
         <BottomNav />
       </div>
     </div>
